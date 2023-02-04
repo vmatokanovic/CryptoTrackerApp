f9d48aa07db8f3a26cd3a8f890c7ec93609bcf77
Valentin Matokanovic <vmatokanovic@etfos.hr>
2023-02-04 14:34:44 +0100

    add bottom tab navigator

--- navigation/AppStack.js
+++ navigation/AppStack.js
@@ -1,14 +1,90 @@
 import React from 'react';
+
 import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+import {Home, Favourites, Search, Profile} from '../screens';
+import { TabIcon } from '../components';
+import { COLORS, icons } from '../constants';
+
 import HomeScreen from '../screens/HomeScreen';
 
-const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
 
 const AppStack = () => {
   return (
-    <Stack.Navigator>
-        <Stack.Screen name = 'Home' component={HomeScreen}/>
-    </Stack.Navigator>
+    <Tab.Navigator
+            screenOptions={{
+              tabBarShowLabel: false,
+                tabBarStyle: {
+                    height: 100,
+                    display: 'flex',
+                    backgroundColor: COLORS.primary,
+                    borderTopColor: "transparent",
+                }
+            }}
+        >
+            <Tab.Screen
+                name="Home"
+                component={Home}
+                options={{
+                  tabBarIcon: ({focused}) => {
+                    return(
+                      <TabIcon 
+                        focused={focused}
+                        icon={icons.home}
+                        label="Home"
+                        />
+                    )
+                  }
+                }}
+            />
+            <Tab.Screen
+                name="Search"
+                component={Search}
+                options={{
+                  tabBarIcon: ({focused}) => {
+                    return(
+                      <TabIcon 
+                        focused={focused}
+                        icon={icons.search}
+                        label="Search"
+                        />
+                    )
+                  }
+                }}
+            />
+            <Tab.Screen
+                name="Favourites"
+                component={Favourites}
+                options={{
+                  tabBarIcon: ({focused}) => {
+                    return(
+                      <TabIcon 
+                        focused={focused}
+                        icon={icons.favourites}
+                        label="Favourites"
+                        />
+                    )
+                  }
+                }}
+            />
+            <Tab.Screen
+                name="Profile"
+                component={Profile}
+                options={{
+                  tabBarIcon: ({focused}) => {
+                    return(
+                      <TabIcon 
+                        focused={focused}
+                        icon={icons.profile}
+                        label="Profile"
+                        />
+                    )
+                  }
+                }}
+            />
+        </Tab.Navigator>
   )
 }
 
---
+++ screens/index.js
@@ -0,0 +1,15 @@
+import Home from "./Home"
+import Portfolio from "./Portfolio"
+import Market from "./Market"
+import Profile from "./Profile"
+import Search from "./Search"
+import Favourites from "./Favourites"
+
+export {
+    Home,
+    Portfolio,
+    Market,
+    Profile,
+    Search,
+    Favourites
+}
